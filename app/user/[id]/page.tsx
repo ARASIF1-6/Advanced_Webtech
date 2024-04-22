8902f9685bf67039eb7475901348adb8ad1ddab7
MD Aminur Rahman Asif <141429059+ARASIF1-6@users.noreply.github.com>
2024-04-22 11:09:49 +0600

    Delete app directory

--- app/user/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import SndNavBar from "@/app/second_nav/navbar";
-
-
-
-
-export default function Id({ params }: { params: { id: string }})
-{
-   return <div> Profile: {params.id}  <SndNavBar/> </div>
-}
